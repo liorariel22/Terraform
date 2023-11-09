8350fb345d9bc725d858b6d77bb1100b2e8b94a6
liorariel22 <150386467+liorariel22@users.noreply.github.com>
2023-11-09 21:41:17 +0200

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -56,15 +56,6 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
     version   = "latest"
   }
 
-  #provisioner "local-exec" {
-  #  command = templatefile("${var.host_os}-ssh-script.tpl", {
-  #    hostname     = self.public_ip_address,
-  #    user         = "adminuser",
-  #    identityfile = "~/.ssh/mtcazurekey"
-  #  })
-  #  interpreter = var.host_os == "linux" ? ["bash", "-c"] : ["Powershell", "-Command"]
-  #}
-
   tags = {
     environment = "dev"
   }
