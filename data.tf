f1226777560c900e632f9b61c29b8138529ac94e
liorariel22 <150386467+liorariel22@users.noreply.github.com>
2023-11-09 21:41:40 +0200

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,8 +1,3 @@
-#data "external" "ip_address" {
-#  depends_on = [azurerm_linux_virtual_machine.mtc-vm]
-#  program    = ["bash", "${path.module}/ip-allocation.sh", "mtc-ip", "mtc-resources"]
-#}
-
 data "external" "update_ssh_config" {
   depends_on = [azurerm_linux_virtual_machine.mtc-vm]
 
